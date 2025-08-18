022d0651765be4a7f14faaa6b7ddd0a7c1b4e12c
jay1535 <habbujayanth@gmail.com>
2025-08-18 20:20:42 +0530

    Final touch

--- notify/src/App.jsx
+++ notify/src/App.jsx
@@ -1,6 +1,6 @@
 import React,{useState} from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import './App.css';
+
 import Navbar from './components/Navbar';
 import Home from './components/Home'; 
 import About from './components/About';
